3ce17f25bdd8516276a492cb4f1b7264e52161c8
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-12-13 11:11:16 -0600

    makes horizontal bar chart lacking labels and hover text.

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,34 @@
-d3.json("samples.json", function(data){
-    console.log(data)
+
+d3.json("samples.json").then((data)=> {
+    var i = Math.floor(((Math.random() * 153) + 1));
+    var samp = data.samples[i];
+    var v = d3.select("#selDatasheet")
+   //length of sample is 153
+   console.log(samp);
+   console.log(i);
+   var otuIds = samp.otu_ids.slice(0, 10)
+   var sampVal = samp.sample_values.slice(0, 10)
+   var otuName = samp.otu_labels.slice(0, 10)
+   
+   console.log(otuIds)
+   console.log(sampVal)
+   console.log(otuName)
+   var trace1 = {
+       x: sampVal,
+       y: otuIds,
+       type: "bar",
+       orientation: "h",
+       
+   }
+   data = [trace1]
+   layout = {
+       title: "attempt",
+       label: otuName
+
+   }
+   
+   Plotly.newPlot("bar", data, layout)
 })
-function unpack(rows, index) {
-    return rows.map(function(row) {
-      return row[index];
-    });
-  }
+
+
   
