dda172835b87c12ff36e18823ca862528b91c573
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-12-14 11:10:42 -0600

    attempts at a dropdown.

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,11 @@
 
 d3.json("samples.json").then((data)=> {
+    
+    
+   function init(){
     var i = Math.floor(((Math.random() * 153) + 1));
     var samp = data.samples[i];
-    var v = d3.select("#selDatasheet")
+    
    //length of sample is 153
    console.log(samp);
    console.log(i);
@@ -10,9 +13,11 @@ d3.json("samples.json").then((data)=> {
    var sampVal = samp.sample_values.slice(0, 10)
    var otuName = samp.otu_labels.slice(0, 10)
    
+   
    console.log(otuIds)
    console.log(sampVal)
    console.log(otuName)
+
    var trace1 = {
        x: sampVal,
        y: otuIds,
@@ -20,15 +25,29 @@ d3.json("samples.json").then((data)=> {
        orientation: "h",
        text: otuName
        
+       
    }
    data = [trace1]
    layout = {
        title: "",
-       
-
    }
    
    Plotly.newPlot("bar", data, layout)
+}
+    init()
+
+   d3.selectAll("#selDataset").on("change", updatePlotly);
+    function updatePlotly(){
+        var dropdownMenu = d3.select("#selDataset").node();
+        var dataset = dropdownMenu.property("value");
+        var x = [];
+        var y = [];
+        for (d=0; d<153; d++){
+            if (data
+        }
+
+
+    }
 })
 
 
